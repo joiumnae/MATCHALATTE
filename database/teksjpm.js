f94d6d063d610e79e274d72f4275956c455476a0
Gxyenn <gxyennxlesy@gmail.com>
2025-03-07 05:01:59 +0700

    Add files via upload

---
+++ database/teksjpm.js
@@ -0,0 +1,12 @@
+*XlesyVIP*
+*Creator Gxyenn*
+`Price harga Script XlesyVIP`
+_15K ENC_
+_30K NO ENC_
+> Kelebihan Script
+_Bisa Run di Termux/Panel_.
+_Fitur Lumayan Ada 527_.
+_Dan Lainya_
+
+*_Silahkan Pm JIKALAU Minat_*
+_owner_ : https://wa.me/623877636168?text=Order_Script_Xlesy-VIP
