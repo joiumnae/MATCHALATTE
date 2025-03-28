75c64c12109f752f362f6cba24b47239f1ea2a58
shaa <jjoiudesumnae@gmail.com>
2025-03-28 12:22:08 +0700

    Update XlesySet.js

--- XlesySet.js
+++ XlesySet.js
@@ -1,16 +1,16 @@
 // config.js
 const XlesySet = {
-    owner: ['6283877636168'],
-    packname: 'Xlesy',
-    author: 'Gxyenn',
-    botname: 'XlesyBot',
+    owner: ['6289605352200'],
+    packname: 'Matcha',
+    author: 'wa.me/62895328876970',
+    botname: 'MATCHALATTE',
     listprefix: ['.'],
     listv: ['❀','✿','⚘','々'],
     number_bot: "", // Kalo pake panel bisa masukin nomor di sini, jika belum ambil session. Format: '628xx'
     dana: ['083877636168'],
     gopay: ['083877636168'],
     ovo: ['kosong'],
-    pulsa: ['083877636168'],
+    pulsa: ['62895328876970'],
     qris: ['kosong'],
 };
 
